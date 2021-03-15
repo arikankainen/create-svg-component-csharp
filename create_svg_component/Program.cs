c4108d91a86f7cf78bae533438a00dcb68828d69
Ari Kankainen <ari.kankainen@saunalahti.fi>
2021-03-15 10:05:54 +0200

    improvements

--- create_svg_component/Program.cs
+++ create_svg_component/Program.cs
@@ -6,56 +6,120 @@ namespace create_svg_component
 {
     class Program
     {
-        static readonly int tabSize = 2;
+        static readonly int tabSize = 4;
         static readonly string oneTab = new string(' ', tabSize);
 
         static string templateStart;
         static string templateEnd;
 
+        static string inputFolder = null;
+        static string outputFolder = null;
+        static string moveFolder = null;
+
+        static int processedCount = 0;
+
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            ParseArgs(args);
+
+            if (inputFolder == null || !Directory.Exists(inputFolder))
             {
-                string file = args[0];
+                Console.WriteLine("Error: Input folder not exist.");
+                Environment.Exit(1);
+            }
 
-                if (File.Exists(file))
+            if (outputFolder == null || !Directory.Exists(outputFolder))
+            {
+                Console.WriteLine("Error: Output folder not exist.");
+                Environment.Exit(1);
+            }
+
+            string[] files = GetFiles();
+
+            foreach (string fileName in files)
+            {
+                ParseFile(fileName);
+            }
+
+            Console.WriteLine("Done: " + processedCount + " files processed.");
+        }
+
+        static void ParseArgs(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "--in")
                 {
-                    try
-                    {
-                        string fileName = Path.GetFileNameWithoutExtension(file);
-                        string fileContents = File.ReadAllText(file);
-                        SetTemplates(fileName);
-
-                        string svg = GetSvg(fileContents);
-                        svg = RemoveProperties(svg);
-                        svg = CapitalizeTagFirstLetter(svg);
-                        svg = AddProps(svg);
-                        svg = AddTabs(svg);
-                        svg = CreateComponent(svg);
-
-                        Console.WriteLine(svg);
-                        File.WriteAllText(fileName + "Svg.tsx", svg);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error: " + ex.Message);
-                    }
+                    inputFolder = args[i + 1];
+                    i++;
                 }
-                else
+
+                if (args[i] == "--out")
                 {
-                    Console.WriteLine("Error: input file not found.");
+                    outputFolder = args[i + 1];
+                    i++;
+                }
+
+                if (args[i] == "--move")
+                {
+                    moveFolder = args[i + 1];
+                    i++;
                 }
             }
-            else
+        }
+
+        static string[] GetFiles()
+        {
+            try
             {
-                Console.WriteLine("Error: input file not specified.");
+                string[] files = Directory.GetFiles(inputFolder, "*.svg", SearchOption.TopDirectoryOnly);
+                return files;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+
+        static void ParseFile(string inputFile)
+        {
+            try
+            {
+                string fileName = Path.GetFileNameWithoutExtension(inputFile);
+                string fileContents = File.ReadAllText(inputFile);
+                SetTemplates(fileName);
+
+                string svg = GetSvg(fileContents);
+                svg = RemoveProperties(svg);
+                svg = CapitalizeTagFirstLetter(svg);
+                svg = AddProps(svg);
+                svg = AddTabs(svg);
+                svg = CreateComponent(svg);
+
+                string fileNameWithFolder = Path.Combine(outputFolder, fileName + ".tsx");
+                File.WriteAllText(fileNameWithFolder, svg);
+
+                if (moveFolder != null && Directory.Exists(moveFolder))
+                {
+                    string outFile = Path.Combine(moveFolder, Path.GetFileName(inputFile));
+                    File.Move(inputFile, outFile);
+                }
+
+                Console.WriteLine("Component created: " + fileName + ".tsx");
+                processedCount++;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
 
         static void SetTemplates(string fileName)
         {
-            templateStart = "import * as React from 'react';\nimport Svg, { Path, G, Circle, Rect, Polygon } from 'react-native-svg';\nimport { SvgProps } from '../../types';\n\nconst " + fileName + "Svg: React.FC<SvgProps> = (props) => {\n" + oneTab + "return (\n";
-            templateEnd = oneTab + ");\n};\n\nexport default " + fileName + "Svg;\n";
+            templateStart = "import * as React from 'react';\nimport Svg, { Path, G, Circle, Rect, Polygon } from 'react-native-svg';\nimport { SvgProps } from '../../types';\n\nconst " + fileName + ": React.FC<SvgProps> = (props) => {\n" + oneTab + "return (\n";
+            templateEnd = oneTab + ");\n};\n\nexport default " + fileName + ";\n";
         }
 
         static string GetSvg(string input)
@@ -94,12 +158,12 @@ namespace create_svg_component
                 {
                     if (tag.Substring(1, 1) != "/")
                     {
-                        string capitalizedTag = tag.Substring(0, 2).ToUpper() + tag[2..].ToLower();
+                        string capitalizedTag = tag.Substring(0, 2).ToUpper() + tag[2..];
                         tags[i] = capitalizedTag;
                     }
                     else
                     {
-                        string capitalizedTag = tag.Substring(0, 3).ToUpper() + tag[3..].ToLower();
+                        string capitalizedTag = tag.Substring(0, 3).ToUpper() + tag[3..];
                         tags[i] = capitalizedTag;
                     }
                 }
@@ -120,15 +184,30 @@ namespace create_svg_component
 
         static string AddTabs(string input)
         {
-            int currentTab = tabSize;
+            int currentTab = tabSize * 2;
+            bool increaseNextTabSize = false;
 
             string[] lines = input.Split('\n');
 
             for (int i = 0; i < lines.Length; i++)
             {
                 string line = lines[i];
-                if (line.StartsWith("</")) currentTab -= tabSize;
-                else if (line.StartsWith("<")) currentTab += tabSize;
+
+                if (line.EndsWith("/>"))
+                {
+                    if (increaseNextTabSize) currentTab += tabSize;
+                    increaseNextTabSize = false;
+                }
+                else if (line.StartsWith("</"))
+                {
+                    currentTab -= tabSize;
+                    increaseNextTabSize = false;
+                }
+                else if (line.StartsWith("<"))
+                {
+                    if (increaseNextTabSize) currentTab += tabSize;
+                    increaseNextTabSize = true;
+                }
 
                 if (line.Trim().Length > 1) lines[i] = new string(' ', currentTab) + line;
             }
